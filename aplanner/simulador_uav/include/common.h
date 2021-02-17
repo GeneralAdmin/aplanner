d9eb8536f83879c41d4f1f4666fce9064a912028
viki <aureliokta@gmail.com>
2021-02-17 14:04:15 -0300

    first commit

---
+++ aplanner/simulador_uav/include/common.h
@@ -0,0 +1,27 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <vector>
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Vector3Stamped.h>
+#include <mavros_msgs/Trajectory.h>
+#include <tf/transform_listener.h>
+
+namespace simuav {
+
+}  // namespace simuav
+
+#endif  // COMMON_H
